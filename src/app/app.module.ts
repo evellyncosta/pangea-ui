bd31c6f5efee17dab008eb588e3e7924f16c2bcf
Evellyn Costa <ecosta.inf@gmail.com>
2020-06-30 11:46:24 -0300

    adição da navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlunosPesquisaComponent } from './alunos-pesquisa/alunos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlunosPesquisaComponent
+    AlunosPesquisaComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
